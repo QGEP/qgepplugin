f08dbd8ae54eed7de0fec4064387d5ef0d50bcf9
Matthias Kuhn <matthias@opengis.ch>
2017-01-19 17:44:04 +0100

    Use v prefix in tags

--- scripts/ci/make_release.sh
+++ scripts/ci/make_release.sh
@@ -8,13 +8,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
 
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
-  echo "Deploy now..."
+  VERSION=$(echo "${TRAVIS_TAG}" | sed -e 's/v\([0-9]\.]*\)/\1/')
+  echo "Deploy version ${VERSION} now..."
 
   ${DIR}/scripts/ci/setup_git.sh
   pushd ${DIR}
   git clone git@github.com:QGEP/repository.git
   cd repository
-  ../scripts/make-qgep-release.sh ${TRAVIS_TAG}
+  ../scripts/make-qgep-release.sh ${VERSION}
 
   popd
 fi
