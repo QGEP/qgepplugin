eeb44195233fde92af2ae2c616e5f1d4e1cad692
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-07-10 09:38:33 +0200

    new release process with plugin-ci

--- src/scripts/ci/setup_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-openssl aes-256-cbc -K $encrypted_5d9ba2164160_key -iv $encrypted_5d9ba2164160_iv -in qgep_rsa.enc -out ~/.ssh/id_rsa -d
-chmod 600 ~/.ssh/id_rsa;
-git config --global user.email "qgep@opengis.ch";
-git config --global user.name "QGEP";
-git config --global push.default simple;
