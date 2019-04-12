e2dc24f5eca24da5e44e46a94219bec347c1927d
Matthias Kuhn <matthias@opengis.ch>
2019-04-12 09:42:51 +0200

    Rename flow times to sum up

--- src/processing_provider/sum_up_upstream.py
+++ src/processing_provider/sum_up_upstream.py
@@ -36,6 +36,7 @@ from qgis.core import (
     QgsProcessingContext,
     QgsProcessingException,
     QgsProcessingFeedback,
+    QgsProcessingParameterDefinition,
     QgsProcessingParameterFeatureSink,
     QgsProcessingParameterVectorLayer,
     QgsProcessingParameterExpression,
@@ -49,8 +50,8 @@ from ..tools.qgepnetwork import QgepGraphManager
 
 from PyQt5.QtCore import QCoreApplication, QVariant
 
-__author__ = 'Denis Rouzaud'
-__date__ = '2018-07-19'
+__author__ = 'Matthias Kuhn'
+__date__ = '2019-04-09'
 __copyright__ = '(C) 2018 by OPENGIS.ch'
 
 # This will get replaced with a git SHA1 when you do a git archive
@@ -58,13 +59,13 @@ __copyright__ = '(C) 2018 by OPENGIS.ch'
 __revision__ = '$Format:%H$'
 
 
-class FlowTimesUpstreamAlgorithm(QgepAlgorithm):
+class SumUpUpstreamAlgorithm(QgepAlgorithm):
     """
     """
 
     REACH_LAYER = 'REACH_LAYER'
     WASTEWATER_NODE_LAYER= 'WASTEWATER_NODE_LAYER'
-    FLOWTIMES_EXPRESSION = 'FLOWTIMES_EXPRESSION'
+    VALUE_EXPRESSION = 'VALUE_EXPRESSION'
     REACH_PK_NAME = 'REACH_PK_NAME'
     NODE_PK_NAME = 'NODE_PK_NAME'
     NODE_FROM_FK_NAME = 'NODE_FROM_FK_NAME'
@@ -74,10 +75,10 @@ class FlowTimesUpstreamAlgorithm(QgepAlgorithm):
     OUTPUT = "OUTPUT"
 
     def name(self):
-        return 'qgep_flow_times_upstream'
+        return 'qgep_values_upstream'
 
     def displayName(self):
-        return self.tr('Flow times upstream')
+        return self.tr('Sum up upstream')
 
     def initAlgorithm(self, config=None):
         """Here we define the inputs and output of the algorithm, along
@@ -85,36 +86,41 @@ class FlowTimesUpstreamAlgorithm(QgepAlgorithm):
         """
 
         # The parameters
-        description = self.tr('Reach layer')
-        self.addParameter(QgsProcessingParameterVectorLayer(self.REACH_LAYER, description=description,
-                                                            types=[QgsProcessing.TypeVectorLine], defaultValue='vw_qgep_reach'))
-        description = self.tr('Wastewater node layer')
-        self.addParameter(QgsProcessingParameterVectorLayer(self.WASTEWATER_NODE_LAYER, description=description,
-                                                            types=[QgsProcessing.TypeVector], defaultValue='vw_wastewater_node'))
-        description = self.tr('Flow times expression')
-        self.addParameter(QgsProcessingParameterExpression(self.FLOWTIMES_EXPRESSION, description=description,
-                                                      parentLayerParameterName=self.REACH_LAYER, defaultValue='1'))
+        description = self.tr('Source Value Expression')
+        self.addParameter(QgsProcessingParameterExpression(self.VALUE_EXPRESSION, description=description,
+                                                      parentLayerParameterName=self.REACH_LAYER))
 
-        description = self.tr('Primary key field reach')
-        self.addParameter(QgsProcessingParameterField(self.REACH_PK_NAME, description=description,
+        description = self.tr('Reach Layer')
+        self.addAdvancedParameter(QgsProcessingParameterVectorLayer(self.REACH_LAYER, description=description,
+                                                            types=[QgsProcessing.TypeVectorLine], defaultValue='vw_qgep_reach'))
+        description = self.tr('Wastewater Node Layer')
+        self.addAdvancedParameter(QgsProcessingParameterVectorLayer(self.WASTEWATER_NODE_LAYER, description=description,
+                                                            types=[QgsProcessing.TypeVector], defaultValue='vw_wastewater_node'))
+
+        description = self.tr('Primary Key Field Reach')
+        self.addAdvancedParameter(QgsProcessingParameterField(self.REACH_PK_NAME, description=description,
                                                       parentLayerParameterName=self.REACH_LAYER, defaultValue='obj_id'))
 
-        description = self.tr('Primary key field node')
-        self.addParameter(QgsProcessingParameterField(self.NODE_PK_NAME, description=description,
+        description = self.tr('Primary Key Field Node')
+        self.addAdvancedParameter(QgsProcessingParameterField(self.NODE_PK_NAME, description=description,
                                                       parentLayerParameterName=self.WASTEWATER_NODE_LAYER, defaultValue='obj_id'))
 
-        description = self.tr('Foreign key field from')
-        self.addParameter(QgsProcessingParameterField(self.NODE_FROM_FK_NAME, description=description,
+        description = self.tr('Foreign Key Field From')
+        self.addAdvancedParameter(QgsProcessingParameterField(self.NODE_FROM_FK_NAME, description=description,
                                                            parentLayerParameterName=self.REACH_LAYER,
                                                            defaultValue='rp_from_fk_wastewater_networkelement'))
 
-        description = self.tr('Foreign key field to')
-        self.addParameter(QgsProcessingParameterField(self.NODE_TO_FK_NAME, description=description,
+        description = self.tr('Foreign Key Field To')
+        self.addAdvancedParameter(QgsProcessingParameterField(self.NODE_TO_FK_NAME, description=description,
                                                            parentLayerParameterName=self.REACH_LAYER,
                                                            defaultValue='rp_to_fk_wastewater_networkelement'))
 
         self.addParameter(QgsProcessingParameterFeatureSink(self.OUTPUT,
-                                                            self.tr('Flow times')))
+                                                            self.tr('Summed up')))
+
+    def addAdvancedParameter(self, parameter):
+        parameter.setFlags(parameter.flags() | QgsProcessingParameterDefinition.FlagAdvanced)
+        self.addParameter(parameter)
 
     def processAlgorithm(self, parameters, context: QgsProcessingContext, feedback: QgsProcessingFeedback):
         """Here is where the processing itself takes place."""
@@ -124,7 +130,7 @@ class FlowTimesUpstreamAlgorithm(QgepAlgorithm):
         # init params
         reach_layer = self.parameterAsVectorLayer(parameters, self.REACH_LAYER, context)
         wastewater_node_layer = self.parameterAsVectorLayer(parameters, self.WASTEWATER_NODE_LAYER, context)
-        flow_time_expression = self.parameterAsExpression(parameters, self.FLOWTIMES_EXPRESSION, context)
+        value_expression = self.parameterAsExpression(parameters, self.VALUE_EXPRESSION, context)
         reach_pk_name = self.parameterAsFields(parameters, self.REACH_PK_NAME, context)[0]
         node_pk_name = self.parameterAsFields(parameters, self.NODE_PK_NAME, context)[0]
         node_from_fk_name = self.parameterAsFields(parameters, self.NODE_FROM_FK_NAME, context)[0]
@@ -132,7 +138,7 @@ class FlowTimesUpstreamAlgorithm(QgepAlgorithm):
 
         # create feature sink
         fields = wastewater_node_layer.fields()
-        fields.append(QgsField('flow_time', QVariant.Double))
+        fields.append(QgsField('value', QVariant.Double))
         (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT, context, fields,
                                                QgsWkbTypes.Point, reach_layer.sourceCrs())
         if sink is None:
@@ -142,14 +148,14 @@ class FlowTimesUpstreamAlgorithm(QgepAlgorithm):
 
         reaches = dict()
 
-        expression = QgsExpression(flow_time_expression)
+        expression = QgsExpression(value_expression)
         context = QgsExpressionContext(QgsExpressionContextUtils.globalProjectLayerScopes(reach_layer))
         expression.prepare(context)
 
         for reach in reach_layer.getFeatures(QgsFeatureRequest()):
             context.setFeature(reach)
-            flow_time = expression.evaluate(context)
-            reaches[reach[node_from_fk_name]] = (reach[node_from_fk_name], reach[node_to_fk_name], flow_time)
+            value = expression.evaluate(context)
+            reaches[reach[node_from_fk_name]] = (reach[node_from_fk_name], reach[node_to_fk_name], value)
 
         qgis.reaches = reaches
 
@@ -177,7 +183,7 @@ class FlowTimesUpstreamAlgorithm(QgepAlgorithm):
 
             new_node = QgsFeature(node)
             new_node.setFields(fields)
-            new_node['flow_time'] = time
+            new_node['value'] = time
 
             sink.addFeature(new_node, QgsFeatureSink.FastInsert)
             feedback.setProgress(current_feature/feature_count * 100)
