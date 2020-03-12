a7bd5306daae29f34aad4513830261eb89a89c16
tproduit <timothee.produit@gmail.com>
2020-03-12 11:04:15 +0100

    Update qgepplugin/processing_provider/provider.py
    Co-Authored-By: Matthias Kuhn <matthias@opengis.ch>

--- qgepplugin/processing_provider/provider.py
+++ qgepplugin/processing_provider/provider.py
@@ -87,7 +87,7 @@ class QgepProcessingProvider(QgsProcessingProvider):
         ProcessingConfig.settingIcons[self.name()] = self.icon()
         ProcessingConfig.addSetting(Setting(self.name(),
                                             'SWMM_PATH',
-                                            self.tr("SWMM executable"),
+                                            self.tr(r'SWMM executable (e.g. C:\Program Files (x86)\EPA SWMM 5.1.013\swmm55.exe)'),
                                             None,
                                             valuetype=Setting.FILE))
 
