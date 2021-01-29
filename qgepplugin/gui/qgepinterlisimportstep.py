23aee7f57640ab29c2df32e7de0d4efde0c87ff8
olivierdalang <olivier.dalang@gmail.com>
2021-01-29 17:03:40 +0100

    detail pane stub

--- qgepplugin/gui/qgepinterlisimportstep.py
+++ qgepplugin/gui/qgepinterlisimportstep.py
@@ -16,24 +16,28 @@ class QgepInterlisImportStepDialog(QDialog, get_ui_class('qgepinterlisimportstep
         super().__init__(parent)
         self.setupUi(self)
 
-        self.treeWidget.itemChanged.connect(self.propagate_item_changed)
+        self.treeWidget.itemChanged.connect(self.item_changed)
+        self.treeWidget.currentItemChanged.connect(self.current_item_changed)
 
     def execute_for_session(self, session: Session):
-        self.treeWidget.clear()
+        """
+        Populates the dialog with data from a session, and executes the dialog allowing to filter the rows to import
+        """
 
-        category_items = defaultdict(QTreeWidgetItem)
-        instances_items = defaultdict(QTreeWidgetItem)
-
+        self.category_items = defaultdict(QTreeWidgetItem)
+        self.instances_items = defaultdict(QTreeWidgetItem)
         bold_font = QFont(QFont().defaultFamily(), weight=QFont.Weight.Bold)
 
+        # Populate the tree widget
+        self.treeWidget.clear()
         for obj in session:
             cls = obj.__class__
 
-            if cls not in category_items:
-                category_items[cls].setText(0, cls.__name__)
-                category_items[cls].setCheckState(0, Qt.Checked)
-                category_items[cls].setFont(0, bold_font)
-                self.treeWidget.addTopLevelItem(category_items[cls])
+            if cls not in self.category_items:
+                self.category_items[cls].setText(0, cls.__name__)
+                self.category_items[cls].setCheckState(0, Qt.Checked)
+                self.category_items[cls].setFont(0, bold_font)
+                self.treeWidget.addTopLevelItem(self.category_items[cls])
 
             status_names = []
             for status_name in ['transient', 'pending', 'persistent', 'deleted', 'detached', 'modified', 'expired']:
@@ -41,27 +45,32 @@ class QgepInterlisImportStepDialog(QDialog, get_ui_class('qgepinterlisimportstep
                     status_names.append(status_name)
             status_disp = ' '.join(status_names)
 
-            instances_items[obj].setText(0, obj.obj_id)
-            instances_items[obj].setText(1, status_disp)
-            instances_items[obj].setCheckState(0, Qt.Checked)
-            category_items[cls].addChild(instances_items[obj])
+            self.instances_items[obj].setText(0, obj.obj_id)
+            self.instances_items[obj].setText(1, status_disp)
+            self.instances_items[obj].setCheckState(0, Qt.Checked)
+            self.category_items[cls].addChild(self.instances_items[obj])
 
         # Show counts
-        for category_item in category_items.values():
+        for category_item in self.category_items.values():
             category_item.setText(0, f"{category_item.text(0)} ({category_item.childCount()})")
 
-        # Exectute
-        accepted = self.exec_()
-        if accepted:
+        # Execute the dialog
+        if self.exec_():
             # Expunge unchecked objects form the session
             for obj in session:
-                if instances_items[obj].checkState(0) != Qt.Checked:
+                if self.instances_items[obj].checkState(0) != Qt.Checked:
                     session.expunge(obj)
+            # User confirmed, we return True to indicate we want a commit
             return True
         else:
+            # User canceled, we return False to indicate we want a rollback
             return False
 
-    def propagate_item_changed(self, item, column):
+    def item_changed(self, item, column):
+        """
+        Propagate checkboxes to parent/children
+        """
+
         checked_state = item.checkState(0)
 
         if checked_state == Qt.PartiallyChecked:
@@ -93,3 +102,21 @@ class QgepInterlisImportStepDialog(QDialog, get_ui_class('qgepinterlisimportstep
             else:
                 # no children at all !!
                 parent.setCheckState(0, Qt.PartiallyChecked)
+
+    def current_item_changed(self, current_item, previous_item):
+        """
+        Populates the details pane
+        """
+
+        self.detailsTextEdit.clear()
+        obj = None
+        for obj, item in self.instances_items.items():
+            if item == current_item:
+                break
+
+        if obj:
+            for c in inspect(obj).mapper.column_attrs:
+                val = getattr(obj, c.key)
+                self.detailsTextEdit.append(f"{c.key}: {val}")
+        else:
+            self.detailsTextEdit.append("Not found")
