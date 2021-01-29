2e48cd668b6490b89739a62051c22708eb0ca587
olivierdalang <olivier.dalang@gmail.com>
2021-01-29 16:41:38 +0100

    basic import filtering

--- qgepplugin/gui/qgepinterlisimportstep.py
+++ qgepplugin/gui/qgepinterlisimportstep.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import inspect
+from collections import defaultdict
 
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QFont
@@ -20,17 +21,18 @@ class QgepInterlisImportStepDialog(QDialog, get_ui_class('qgepinterlisimportstep
     def execute_for_session(self, session: Session):
         self.treeWidget.clear()
 
-        category_items = {}
+        category_items = defaultdict(QTreeWidgetItem)
+        instances_items = defaultdict(QTreeWidgetItem)
+
+        bold_font = QFont(QFont().defaultFamily(), weight=QFont.Weight.Bold)
 
         for obj in session:
             cls = obj.__class__
+
             if cls not in category_items:
-                category_items[cls] = QTreeWidgetItem([cls.__name__])
-                # category_items[cls].setFlags(Qt.ItemIsSelectable | Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
-                category_items[cls].setCheckState(2, Qt.Checked)
-                font = QFont(category_items[cls].font(0))
-                font.setBold(True)
-                category_items[cls].setFont(0, font)
+                category_items[cls].setText(0, cls.__name__)
+                category_items[cls].setCheckState(0, Qt.Checked)
+                category_items[cls].setFont(0, bold_font)
                 self.treeWidget.addTopLevelItem(category_items[cls])
 
             status_names = []
@@ -39,26 +41,35 @@ class QgepInterlisImportStepDialog(QDialog, get_ui_class('qgepinterlisimportstep
                     status_names.append(status_name)
             status_disp = ' '.join(status_names)
 
-            item = QTreeWidgetItem([obj.obj_id, status_disp])
-            item.setCheckState(2, Qt.Checked)
-            # item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
-            category_items[cls].addChild(item)
+            instances_items[obj].setText(0, obj.obj_id)
+            instances_items[obj].setText(1, status_disp)
+            instances_items[obj].setCheckState(0, Qt.Checked)
+            category_items[cls].addChild(instances_items[obj])
 
         # Show counts
         for category_item in category_items.values():
             category_item.setText(0, f"{category_item.text(0)} ({category_item.childCount()})")
 
-        return self.exec_()
+        # Exectute
+        accepted = self.exec_()
+        if accepted:
+            # Expunge unchecked objects form the session
+            for obj in session:
+                if instances_items[obj].checkState(0) != Qt.Checked:
+                    session.expunge(obj)
+            return True
+        else:
+            return False
 
     def propagate_item_changed(self, item, column):
-        checked_state = item.checkState(2)
+        checked_state = item.checkState(0)
 
         if checked_state == Qt.PartiallyChecked:
             return
 
         # propagate to children
         for child in [item.child(i) for i in range(item.childCount())]:
-            child.setCheckState(2, checked_state)
+            child.setCheckState(0, checked_state)
 
         # propagate to parent
         parent = item.parent()
@@ -66,19 +77,19 @@ class QgepInterlisImportStepDialog(QDialog, get_ui_class('qgepinterlisimportstep
             has_checked = False
             has_unchecked = False
             for sibling in [parent.child(i) for i in range(parent.childCount())]:
-                if sibling.checkState(2) == Qt.Checked:
+                if sibling.checkState(0) == Qt.Checked:
                     has_checked = True
-                if sibling.checkState(2) == Qt.Unchecked:
+                if sibling.checkState(0) == Qt.Unchecked:
                     has_unchecked = True
                 if has_checked and has_unchecked:
                     break
 
             if has_checked and has_unchecked:
-                parent.setCheckState(2, Qt.PartiallyChecked)
+                parent.setCheckState(0, Qt.PartiallyChecked)
             elif has_checked:
-                parent.setCheckState(2, Qt.Checked)
+                parent.setCheckState(0, Qt.Checked)
             elif has_unchecked:
-                parent.setCheckState(2, Qt.Unchecked)
+                parent.setCheckState(0, Qt.Unchecked)
             else:
                 # no children at all !!
-                parent.setCheckState(2, Qt.PartiallyChecked)
+                parent.setCheckState(0, Qt.PartiallyChecked)
