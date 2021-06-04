47097a983ea01fdfb56689019bb63eee4d318409
olivierdalang <olivier.dalang@gmail.com>
2021-06-04 17:12:12 +0200

    (precommit)

--- qgepplugin/gui/qgepdatamodeldialog.py
+++ qgepplugin/gui/qgepdatamodeldialog.py
@@ -229,7 +229,9 @@ class QgepDatamodelInitToolDialog(QDialog, get_ui_class("qgepdatamodeldialog.ui"
 
     def _show_progress(self, message):
         if self.progress_dialog is None:
-            self.progress_dialog = QProgressDialog(self.tr("Starting..."), self.tr("Cancel"), 0, 0)
+            self.progress_dialog = QProgressDialog(
+                self.tr("Starting..."), self.tr("Cancel"), 0, 0
+            )
             cancel_button = QPushButton(self.tr("Cancel"))
             cancel_button.setEnabled(False)
             self.progress_dialog.setCancelButton(cancel_button)
